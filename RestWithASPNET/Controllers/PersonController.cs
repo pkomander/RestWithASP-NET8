1ad2aa4816b7886481f791d5d88e4a8822860793
GeoexPaulo <praraujo2010@hotmail.com>
2024-06-29 13:00:17 -0300

    criando classe Person e controller.

---
+++ RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
